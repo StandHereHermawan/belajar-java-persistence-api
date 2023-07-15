993c018a24baf7ba34526c64b2533e1c1614725a
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-15 18:39:27 +0700

    Initial commit

---
+++ src/test/java/ariefbelajarjava/persistenceapi/BelajarJavaPersistenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ariefbelajarjava.persistenceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarJavaPersistenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
