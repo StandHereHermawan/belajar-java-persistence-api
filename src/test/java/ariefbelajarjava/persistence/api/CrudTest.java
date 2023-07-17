d5c56bc937de03f2b1ce8d430ff8e132fb70906c
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-17 22:19:32 +0700

    Belajar Java Persistence API, CRUD, CrudTest insert data ke database

---
+++ src/test/java/ariefbelajarjava/persistence/api/CrudTest.java
@@ -0,0 +1,35 @@
+package ariefbelajarjava.persistence.api;
+
+import ariefbelajarjava.persistence.api.entity.Customer;
+import ariefbelajarjava.persistence.api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CrudTest {
+
+    private EntityManagerFactory entityManagerFactory;
+
+    @BeforeEach
+    void setUp() {
+        entityManagerFactory = JpaUtil.getEntityManagerFactory();
+    }
+
+    @Test
+    void insert() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId("1");
+        customer.setName("Arief");
+
+        entityManager.persist(customer);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
