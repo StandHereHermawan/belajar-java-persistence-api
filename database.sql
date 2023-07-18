5abd79010e7520ebf7752545fca0677314dd9c69
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-19 02:16:53 +0700

    Belajar Java Persistence API, Generated Value, Membuat tabel categories

--- database.sql
+++ database.sql
@@ -2,12 +2,23 @@ CREATE DATABASE belajar_java_persistence_api;
 
 use belajar_java_persistence_api;
 
-CREATE TABLE customers (
-    id VARCHAR(255) not null primary key ,
+CREATE TABLE customers
+(
+    id   VARCHAR(255) not null primary key,
     name VARCHAR(100) not null
-)ENGINE = InnoDB;
+) ENGINE = InnoDB;
 
-SELECT * FROM customers;
+SELECT *
+FROM customers;
 
 ALTER TABLE customers
-add column primary_email varchar(150);
+    add column primary_email varchar(150);
+
+CREATE TABLE categories
+(
+    id          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name        VARCHAR(100) not null,
+    description VARCHAR(500)
+) engine = InnoDB;
+
+SELECT * FROM categories;
