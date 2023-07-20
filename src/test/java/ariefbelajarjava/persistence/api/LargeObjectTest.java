e0c91c2f6d5b861a306d4e8a986412ce187cdde5
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-21 01:09:25 +0700

    Belajar Java Persistence API, Large Object, LargeObject Test, dapet error path

---
+++ src/test/java/ariefbelajarjava/persistence/api/LargeObjectTest.java
@@ -0,0 +1,36 @@
+package ariefbelajarjava.persistence.api;
+
+import ariefbelajarjava.persistence.api.entity.Image;
+import ariefbelajarjava.persistence.api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class LargeObjectTest {
+
+    @Test
+    void largeObject() throws IOException {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Image image = new Image();
+        image.setName("Contoh Image");
+        image.setDescription("Contoh deskripsi Image");
+
+        byte[] bytes = Files.readAllBytes(Path.of(getClass().getResource("/images/troll.png")));
+
+        image.setImage(bytes);
+
+        entityManager.persist(image);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
