9d79f28816033758ec29706f2e5acdf1f7375030
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-23 07:14:10 +0700

    Belajar Java Persistence API, Embedded Id, add Embeddable DepartmentId class

---
+++ src/main/java/ariefbelajarjava/persistence/api/entity/DepartmentId.java
@@ -0,0 +1,30 @@
+package ariefbelajarjava.persistence.api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class DepartmentId {
+
+    @Column(name = "company_id")
+    private String companyId;
+
+    @Column(name = "department_id")
+    private String departmentId;
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+}
