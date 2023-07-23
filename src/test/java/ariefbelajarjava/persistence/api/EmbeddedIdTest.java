b90183d00284144e0b77cac4e87e4fab765bd929
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-23 07:34:32 +0700

    Belajar Java Persistence API, Embedded Id, EmbeddedId Create Operation Test

---
+++ src/test/java/ariefbelajarjava/persistence/api/EmbeddedIdTest.java
@@ -0,0 +1,35 @@
+package ariefbelajarjava.persistence.api;
+
+import ariefbelajarjava.persistence.api.entity.Department;
+import ariefbelajarjava.persistence.api.entity.DepartmentId;
+import ariefbelajarjava.persistence.api.entity.Member;
+import ariefbelajarjava.persistence.api.entity.Name;
+import ariefbelajarjava.persistence.api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+
+public class EmbeddedIdTest {
+
+    @Test
+    void embeddedId() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        DepartmentId id = new DepartmentId();
+        id.setCompanyId("Belum ada Company");
+        id.setDepartmentId("Belum ada");
+
+        Department department = new Department();
+        department.setId(id);
+        department.setName("Belum ada Department");
+
+        entityManager.persist(department);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
