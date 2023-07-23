b4b9561ef82e1d25be7a627298be51507f3af675
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-24 04:38:59 +0700

    Belajar Java Persistence API, Collection, Collection Test

---
+++ src/test/java/ariefbelajarjava/persistence/api/CollectionTest.java
@@ -0,0 +1,39 @@
+package ariefbelajarjava.persistence.api;
+
+import ariefbelajarjava.persistence.api.entity.Member;
+import ariefbelajarjava.persistence.api.entity.Name;
+import ariefbelajarjava.persistence.api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CollectionTest {
+
+    @Test
+    void collection() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Name name = new Name();
+        name.setFirstName("Arief");
+        name.setMiddleName("Karditya");
+        name.setLastName("Hermawan");
+
+        Member member = new Member();
+        member.setEmail("test@example.com");
+        member.setName(name);
+        member.setHobbies(new ArrayList<>());
+        member.getHobbies().add("Nonton Youtube");
+        member.getHobbies().add("Maen Warthunder");
+
+        entityManager.persist(member);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
