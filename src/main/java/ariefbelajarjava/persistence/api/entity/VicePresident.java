3c4bd62ad40268acb5ed40a9432264b2b9ee0fd1
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-28 15:25:30 +0700

    Belajar Java Persistence API, Inheritance "Single Table Inheritance", add VicePresident class

---
+++ src/main/java/ariefbelajarjava/persistence/api/entity/VicePresident.java
@@ -0,0 +1,21 @@
+package ariefbelajarjava.persistence.api.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("VP")
+public class VicePresident extends Employee {
+
+    @Column(name = "total_manager")
+    private Integer totalManager;
+
+    public Integer getTotalManager() {
+        return totalManager;
+    }
+
+    public void setTotalManager(Integer totalManager) {
+        this.totalManager = totalManager;
+    }
+}
