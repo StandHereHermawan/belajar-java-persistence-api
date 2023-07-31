40425982f9cb1791145e19420d465045a46ff9c0
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-31 09:26:03 +0700

    Belajar Java Persistence API, Mapped Superclass, MappedSuperClassTest Insert

---
+++ src/test/java/ariefbelajarjava/persistence/api/MappedSuperClassTest.java
@@ -0,0 +1,33 @@
+package ariefbelajarjava.persistence.api;
+
+import ariefbelajarjava.persistence.api.entity.Brand;
+import ariefbelajarjava.persistence.api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class MappedSuperClassTest {
+
+    @Test
+    void mappedSuperClass() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Brand brand = new Brand();
+        brand.setId("xiaomi");
+        brand.setName("Xiaomi");
+        brand.setDescription("Xiaomi Global");
+        brand.setCreatedAt(LocalDateTime.now());
+        brand.setUpdatedAt(LocalDateTime.now());
+
+        entityManager.persist(brand);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
