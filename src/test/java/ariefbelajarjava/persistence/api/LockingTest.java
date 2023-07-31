3cc08db490365984837b6bcce4c4da117c141be4
StandHereHermawan <nolifegansaya@gmail.com>
2023-07-31 09:53:12 +0700

    Belajar Java Persistence API, Optimistic Locking, LockingTest versionTest

---
+++ src/test/java/ariefbelajarjava/persistence/api/LockingTest.java
@@ -0,0 +1,32 @@
+package ariefbelajarjava.persistence.api;
+
+import ariefbelajarjava.persistence.api.entity.Brand;
+import ariefbelajarjava.persistence.api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class LockingTest {
+
+    @Test
+    void versionTest() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Brand brand = new Brand();
+        brand.setId("nokia");
+        brand.setName("Nokia");
+        brand.setDescription("Nokia");
+        brand.setCreatedAt(LocalDateTime.now());
+        brand.setUpdatedAt(LocalDateTime.now());
+        entityManager.persist(brand);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
