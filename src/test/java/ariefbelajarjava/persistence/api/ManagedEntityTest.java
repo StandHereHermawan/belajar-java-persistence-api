6c554d685afde633f54e22f941cb57a2a13f9727
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-01 12:47:38 +0700

    Belajar Java Persistence API, Managed Entity, ManagedEntityTest

---
+++ src/test/java/ariefbelajarjava/persistence/api/ManagedEntityTest.java
@@ -0,0 +1,31 @@
+package ariefbelajarjava.persistence.api;
+
+import ariefbelajarjava.persistence.api.entity.Brand;
+import ariefbelajarjava.persistence.api.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+
+public class ManagedEntityTest {
+
+    @Test
+    void managedEntityCreateUpdate() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        // unmanaged entity
+        Brand brand = new Brand();
+        brand.setId("apple");
+        brand.setName("Apple");
+        entityManager.persist(brand); // managed entity
+
+        brand.setName("Apple International"); // auto update karena sudah jadi managed entity
+        // entityManager.merge(brand);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
