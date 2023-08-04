43c590daa00e7372f8a2f7278a33ac0affbf72d3
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-04 09:31:28 +0700

    Belajar Java Persistence API, Criteria Select Non Entity, Criteria query non entity Test

--- src/test/java/ariefbelajarjava/persistence/api/CriteriaTest.java
+++ src/test/java/ariefbelajarjava/persistence/api/CriteriaTest.java
@@ -37,4 +37,29 @@ public class CriteriaTest {
         entityTransaction.commit();
         entityManager.close();
     }
+
+    @Test
+    void criteriaQueryNonEntity() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Brand> b = criteriaQuery.from(Brand.class);
+        criteriaQuery.select(criteriaBuilder.array(b.get("id"),b.get("name")));
+        // select b.id, b.name from Brand b
+
+        TypedQuery<Object[]> query = entityManager.createQuery(criteriaQuery);
+        List<Object[]> objects = query.getResultList();
+        for (Object[] object : objects) {
+            System.out.println("Id : "+object[0]);
+            System.out.println("Name : "+object[1]);
+        }
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
 }
